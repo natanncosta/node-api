cc450b5a018360b7fe742859e026d2c98ae413be
natan <nataniel08@protonmail.com>
2023-01-06 18:14:27 -0300

    feat: ensure EncryptAdapter throw if bcrypt throws

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt'
+import { Encrypter } from '../../data/protocols/encrypter'
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt (password: string): Promise<string> {
+    const hash = await bcrypt.hash(password, 12)
+    return hash
+  }
+}
